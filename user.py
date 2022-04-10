ff60dbd925ee7b0877b1fabbd5e13352a083e792
ehhaines <haines.e@northeastern.edu>
2022-04-10 01:09:24 -0400

    Created login function

--- user.py
+++ user.py
@@ -1,3 +1,8 @@
+'''
+None -> (String, String, String, String)
+
+Gets the login information of a user and returns it as a tuple.
+'''
 def get_credentials():
 
   first = input("First Name: ")
@@ -8,11 +13,43 @@ def get_credentials():
   return first, last, email, pwd
 
 
+'''
+DictCursor -> bool
+
+Calls the 'create user' stored procedure in ncr database to insert a user. Needs a cursor object to
+access the database. Returns True if the account is successfully created. Otherwise False.
+'''
 def create_account(cursor):
 
   cred = get_credentials()
-
   valid = "northeastern.edu" in cred[2] and len(cred[0]) > 0 and len(cred[1]) > 0 and len(cred[3]) > 0
 
   if valid:
-    cursor.callproc("create_user")
+    try:
+      cursor.callproc("create_user", cred)
+    except Exception as e:
+      valid = False
+      print(e)
+
+  return valid
+
+
+'''
+DictCursor -> int
+
+Calls the 'login' stored function to count the number of users that match the
+user-input email and password. Returns this number.
+'''
+def login(cursor):
+
+  email = input("Email: ")
+  pwd = input("Password: ")
+  num_rows = 0
+
+  try:
+    num_rows = cursor.callproc("login", (email, pwd))
+  except Exception as e:
+    print(e + "\n")
+    print("Something went wrong trying to log you in...")
+  
+  return num_rows
